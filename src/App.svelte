27ddcefc54105795a1facf7ecfa6fc3a5526677c
Niklas20114552 <niklas.deppermann@outlook.com>
2024-10-11 17:26:12 +0200

    Added basic login mechanic

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script>
     import "bootstrap/dist/css/bootstrap.min.css";
     import "bootstrap/dist/js/bootstrap.js";
+    import { sha256 } from "barely-sha256";
+    import { MoonStarsFill, BrightnessHighFill } from "svelte-bootstrap-icons";
     import { onMount } from "svelte";
 
     let darkMode = localStorage.getItem("darkMode") != "false";
@@ -14,6 +16,46 @@
     });
 
     $: updateTheme(darkMode);
+
+    let page_state = "loading";
+    const websocket = new WebSocket(
+        `${location.protocol === "https:" ? "wss" : "ws"}://${location.hostname}:29836`,
+    );
+
+    websocket.onerror = () => {
+        page_state = "error";
+    };
+    websocket.onclose = () => {
+        page_state = "error";
+    };
+    websocket.onopen = () => {
+        page_state = "login";
+    };
+    websocket.onmessage = (message) => {
+        const jdata = JSON.parse(message.data);
+        console.log(jdata);
+        switch (jdata.data) {
+            case "welcome":
+                page_state = "server";
+            case "exception":
+                switch (jdata.msg) {
+                    case "invalid login":
+                        document.getElementById("pass_input").value = "";
+                        document.getElementById("pass_text").innerText =
+                            "Your password is incorrect!\nIf you forgot your password, reset it using the CLI tool on this server.";
+                }
+        }
+    };
+    function passEntered(d) {
+        if (d.key == "Enter") {
+            websocket.send(
+                JSON.stringify({
+                    data: "auth",
+                    hash: sha256(document.getElementById("pass_input").value),
+                }),
+            );
+        }
+    }
 </script>
 
 <main>
@@ -40,45 +82,41 @@
                         >
                     </li>
                 </ul>
+                <button
+                    class="btn btn-outline-secondary"
+                    type="button"
+                    on:click={() => (darkMode = !darkMode)}
+                >
+                    {#if darkMode}
+                        <BrightnessHighFill />
+                    {:else}
+                        <MoonStarsFill />
+                    {/if}
+                </button>
             </div>
-            <button
-                class="btn btn-outline-secondary"
-                type="button"
-                on:click={() => (darkMode = !darkMode)}
-            >
-                {#if darkMode}
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        class="bi bi-brightness-high-fill"
-                        viewBox="0 0 16 16"
-                    >
-                        <path
-                            d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708"
-                        />
-                    </svg>
-                {:else}
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        class="bi bi-moon-stars-fill"
-                        viewBox="0 0 16 16"
-                    >
-                        <path
-                            d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278"
-                        />
-                        <path
-                            d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.73 1.73 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.73 1.73 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.73 1.73 0 0 0 1.097-1.097zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.16 1.16 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.16 1.16 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732z"
-                        />
-                    </svg>
-                {/if}
-            </button>
         </div>
     </nav>
+    {#if page_state == "loading"}
+        <div class="position-absolute top-50 start-50 translate-middle fs-1">
+            Please wait...
+        </div>
+    {:else if page_state == "error"}
+        <div class="position-absolute top-50 start-50 translate-middle fs-1">
+            Failed to connect to server!
+        </div>
+    {:else if page_state == "login"}
+        <div class="position-absolute top-50 start-50 translate-middle">
+            <input
+                id="pass_input"
+                class="form-control"
+                type="password"
+                placeholder="Enter your password here"
+                aria-label="password input"
+                on:keydown={passEntered}
+            />
+            <div id="pass_text" class="form-text text-danger" />
+        </div>
+    {/if}
 </main>
 
 <style>
